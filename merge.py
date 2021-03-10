ec80a9f1e790dc211073960a84070d9c5ddaca26
Chien <xiangjianjian@aliyun.com>
2021-03-10 12:35:42 +0800

    update

--- merge.py
+++ merge.py
@@ -1,13 +1,12 @@
 import os
 
 
-def get_index(file):
+def get_index(lines):
     index = 0
-    lines = file.readlines()
     for line in lines:
         index += 1
         if line == '[Rule]\n':
-            return index, lines
+            return index
 
 
 if __name__ == '__main__':
@@ -15,8 +14,10 @@ if __name__ == '__main__':
     file_1 = open(parent_path + '/Complete.conf', mode='r')
     file_2 = open(parent_path + '/UnblockNeteaseMusic.conf', mode='r')
     file_merge = open(parent_path + '/merge.conf', mode='w')
-    index1, lines_1 = get_index(file_1)
-    index2, lines_2 = get_index(file_2)
+    lines_1 = file_1.readlines()
+    lines_2 = file_2.readlines()
+    index1 = get_index(lines_1)
+    index2 = get_index(lines_2)
 
     for i in range(0, index1):
         file_merge.write(lines_1[i])
@@ -24,8 +25,16 @@ if __name__ == '__main__':
     for i in range(index2, len(lines_2) - 2):
         file_merge.write(lines_2[i])
 
+    flag_1 = False
     for i in range(index1, len(lines_1)):
-        file_merge.write(lines_1[i])
+        if (lines_1[i] == '# > Netease Music\n'):
+            flag_1 = True
+
+        if (flag_1 and len(lines_1[i]) <= 1):
+            flag_1 = False
+
+        if (not flag_1):
+            file_merge.write(lines_1[i])
 
     file_1.close()
     file_2.close()
