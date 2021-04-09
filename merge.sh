19f8e3a92f29678ad5267bade3361c9b7330ec39
Chien <xiangjianjian@aliyun.com>
2021-04-09 20:53:47 +0800

    update

--- merge.sh
+++ merge.sh
@@ -4,8 +4,8 @@ rm ./Complete.conf
 rm ./sr_top500_banlist_ad.conf
 wget https://cdn.jsdelivr.net/gh/lhie1/Rules/Shadowrocket/Complete.conf
 wget https://h2y.github.io/Shadowrocket-ADBlock-Rules/sr_top500_banlist_ad.conf
-python ./merge-h2y.py
-python ./merge-lhie1.py
+python ./scripts/merge-h2y.py
+python ./scripts/merge-lhie1.py
 
 git config --global user.email xiangjianjian@aliyun.com
 git config --global user.name Chien
