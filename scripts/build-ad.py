4ccf6ce3dca7427bc8ffc141215cee939cc5acb7
Chien <xiangjianjian@aliyun.com>
2021-04-16 13:35:09 +0800

    add ad rule-set

--- scripts/build-ad.py
+++ scripts/build-ad.py
@@ -13,7 +13,10 @@ if __name__ == '__main__':
     ad = get_anti_ad()
     ad = ad.split('\n')
     ad_file = open(os.getcwd() + '/temp/ad.txt', mode='w', encoding='utf-8')
+    ad_snip = open(os.getcwd() + '/snippet/ad.txt', mode='w', encoding='utf-8')
     for line in ad:
         if not line.startswith('#') and len(line) > 0:
-            ad_file.write('DOMAIN,%s,REJECT\n' % line)
+            ad_file.write('DOMAIN-SUFFIX,%s,REJECT\n' % line)
+            ad_snip.write('DOMAIN-SUFFIX,%s\n' % line)
     ad_file.close()
+    ad_snip.close()
