faecc36493f91821f4eae2cfca9250ffe2ca8494
Chien <xiangjianjian@aliyun.com>
2021-04-30 20:59:21 +0800

    manual update

--- scripts/build-ad.py
+++ scripts/build-ad.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_anti_ad():
-    res = requests.get('https://anti-ad.net/domains.txt')
+    res = requests.get('https://anti-ad.net/surge.txt')
     if res.status_code != 200:
         raise Exception('Connect error')
     return res.text
@@ -13,10 +13,7 @@ if __name__ == '__main__':
     ad = get_anti_ad()
     ad = ad.split('\n')
     ad_file = open(os.getcwd() + '/temp/ad.txt', mode='w', encoding='utf-8')
-    ad_snip = open(os.getcwd() + '/snippet/ad.txt', mode='w', encoding='utf-8')
     for line in ad:
         if not line.startswith('#') and len(line) > 0:
-            ad_file.write('DOMAIN-SUFFIX,%s,REJECT\n' % line)
-            ad_snip.write('DOMAIN-SUFFIX,%s\n' % line)
+            ad_file.write('%s,REJECT\n' % line)
     ad_file.close()
-    ad_snip.close()
