da60c331548525fa0a0b8ebf4846c1629c03eda0
Chien <xiangjianjian@aliyun.com>
2021-05-08 10:27:48 +0800

    manual update

--- scripts/manual-update.sh
+++ scripts/manual-update.sh
@@ -2,11 +2,11 @@
 
 rm ./Complete.conf
 wget https://cdn.jsdelivr.net/gh/lhie1/Rules/Shadowrocket/Complete.conf
-python ./scripts/merge-lhie1.py
-python ./scripts/build-gfwlist.py
-python ./scripts/build-ad.py
-python ./scripts/build-telegram.py
-python ./scripts/gen-gfw-ad-netease.py
+python3 ./scripts/merge-lhie1.py
+python3 ./scripts/build-gfwlist.py
+python3 ./scripts/build-ad.py
+python3 ./scripts/build-telegram.py
+python3 ./scripts/gen-gfw-ad-netease.py
 
 git add .
 git commit -m "manual update" -a
