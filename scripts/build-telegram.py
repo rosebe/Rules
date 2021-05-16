b315bdf596a037f08cc0b87e67a6e95090c325fa
Chien <xiangjianjian@aliyun.com>
2021-05-16 16:17:15 +0800

    manual update

--- scripts/build-telegram.py
+++ scripts/build-telegram.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     telegram = telegram.split('\n')
     telegram_file = open(os.getcwd() + '/temp/telegram.txt', mode='w', encoding='utf-8')
     for line in telegram:
-        if len(line) > 0 and not line.startswith('IP-CIDR6'):
+        if len(line) > 0:
+            line = line.replace('IP-CIDR6', 'IP-CIDR')
             telegram_file.write('%s,PROXY,no-resolve\n' % line)
     telegram_file.close()
