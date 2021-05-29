a2357ded1d622fbb26658a97164ba91b8cda7a16
Chien <xiangjianjian@aliyun.com>
2021-05-29 12:48:35 +0800

    add telegram

--- scripts/build-telegram.py
+++ scripts/build-telegram.py
@@ -13,7 +13,10 @@ if __name__ == '__main__':
     telegram = get_telegram()
     telegram = telegram.split('\n')
     telegram_file = open(os.getcwd() + '/temp/telegram.txt', mode='w', encoding='utf-8')
+    telegram_snippet = open(os.getcwd() + '/snippet/telegram.txt', mode='w', encoding='utf-8')
     for line in telegram:
         if len(line) > 0:
             telegram_file.write('%s,PROXY,no-resolve\n' % line.replace('IP-CIDR6', 'IP-CIDR'))
+            telegram_snippet.write('%s\n' % line.replace('IP-CIDR6', 'IP-CIDR'))
     telegram_file.close()
+    telegram_snippet.close()
