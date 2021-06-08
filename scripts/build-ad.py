8591966eaa100871102469d3958f445220035872
Chien <xiangjianjian@aliyun.com>
2021-06-08 15:17:49 +0800

    remove anti-ad

--- scripts/build-ad.py
+++ scripts/build-ad.py
@@ -8,19 +8,12 @@ def get_ad(url):
         raise Exception('Connect error')
     return res.text.split('\n')
 
-anti_ad_url = 'https://anti-ad.net/surge.txt'
 reject_url = 'https://cdn.jsdelivr.net/gh/Loyalsoldier/surge-rules@release/ruleset/reject.txt'
 
 if __name__ == '__main__':
-    anti_ad = get_ad(anti_ad_url)
     reject = get_ad(reject_url)
-    anti_ad = set(anti_ad)
-    reject = set(reject)
-    ad = reject.union(anti_ad)
-    ad = list(ad)
-    ad.sort()
     ad_file = open(os.getcwd() + '/temp/ad.txt', mode='w', encoding='utf-8')
-    for line in ad:
+    for line in reject:
         if not line.startswith('#') and len(line) > 0:
             ad_file.write('%s,REJECT\n' % line)
     ad_file.close()
