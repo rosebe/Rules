9cad73a7eef106a6c84eb144c0866391fbb80e57
Chien <xiangjianjian@hotmail.com>
2021-07-02 19:45:46 +0800

    update merge script

--- scripts/merge-lhie1.py
+++ scripts/merge-lhie1.py
@@ -2,6 +2,7 @@ import os
 import time
 import requests
 
+
 def get_index(lines):
     index = 0
     for line in lines:
@@ -9,8 +10,10 @@ def get_index(lines):
         if line == '[Rule]\n':
             return index
 
+
 def get_lhie1():
-    res = requests.get('https://cdn.jsdelivr.net/gh/lhie1/Rules@master/Shadowrocket/Complete.conf')
+    res = requests.get(
+        'https://cdn.jsdelivr.net/gh/lhie1/Rules@master/Shadowrocket/Complete.conf')
     if res.status_code != 200:
         raise Exception('Connect error')
     return res.text.split('\n')
@@ -26,13 +29,20 @@ if __name__ == '__main__':
     file_merge.write(
         "# Update at " + time.strftime("%Y-%m-%d %H:%M:%S %Z") + "\n")
 
+    flag = True
     for rule_1 in lines_1:
+        if rule_1.startswith('# > Netease Music'):
+            flag = False
+        if not flag and len(rule_1) < 1:
+            flag = True
+
         if rule_1.startswith('GEOIP'):
             file_merge.write('# > Netease Music Unblock\n')
             for rule_2 in lines_2:
                 file_merge.write(rule_2)
             file_merge.write('\n')
-        file_merge.write(rule_1 + '\n')
+        if flag:
+            file_merge.write(rule_1 + '\n')
 
     print('lhie1/Complete.conf merged')
 
